3b409d2d529143c09475866da9082f99fb503827
Jesse <frejes32@gmail.com>
2024-11-19 20:03:40 -0600

    Created repository, service, interfaces, and endpoints for payment types

---
+++ Tech-Trader-Server/Interfaces/IPaymentTypeService.cs
@@ -0,0 +1,13 @@
+﻿using TechTrader.Models;
+
+namespace TechTrader.Interfaces
+{
+    public interface IPaymentTypeService
+    {
+        Task<List<PaymentType>> GetPaymentTypesAsync();
+        Task<PaymentType> GetPaymentTypeByIdAsync(int paymentTypeId);
+        Task<PaymentType> CreatePaymentTypeAsync(PaymentType PaymentType);
+        Task<PaymentType> UpdatePaymentTypeAsync(int paymentTypeId, PaymentType PaymentType);
+        Task<PaymentType> DeletePaymentTypeAsync(int paymentTypeId);
+    }
+}
---
+++ Tech-Trader-Server/Services/PaymentTypeService.cs
@@ -0,0 +1,40 @@
+﻿using TechTrader.Models;
+using TechTrader.Interfaces;
+
+namespace TechTrader.Services
+{
+    public class PaymentTypeService : IPaymentTypeService
+    {
+        private readonly IPaymentTypeRepository _paymentTypeRepository;
+
+        public PaymentTypeService(IPaymentTypeRepository paymentTypeRepository)
+        {
+            _paymentTypeRepository = paymentTypeRepository;
+        }
+
+        public async Task<List<PaymentType>> GetPaymentTypesAsync()
+        {
+            return await _paymentTypeRepository.GetPaymentTypesAsync();
+        }
+
+        public async Task<PaymentType> GetPaymentTypeByIdAsync(int paymentTypeId)
+        {
+            return await _paymentTypeRepository.GetPaymentTypeByIdAsync(paymentTypeId);
+        }
+
+        public async Task<PaymentType> CreatePaymentTypeAsync(PaymentType paymentType)
+        {
+            return await _paymentTypeRepository.CreatePaymentTypeAsync(paymentType);
+        }
+
+        public async Task<PaymentType> UpdatePaymentTypeAsync(int paymentTypeId, PaymentType paymentType)
+        {
+            return await _paymentTypeRepository.UpdatePaymentTypeAsync(paymentTypeId, paymentType);
+        }
+
+        public async Task<PaymentType> DeletePaymentTypeAsync(int paymentTypeId)
+        {
+            return await _paymentTypeRepository.DeletePaymentTypeAsync(paymentTypeId);
+        }
+    }
+}
