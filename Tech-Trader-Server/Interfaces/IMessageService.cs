d1e14754e84c89c1417e8a1fe1c8e09db0c6fa20
Jesse <frejes32@gmail.com>
2024-11-19 20:30:28 -0600

    Created repository, service, interfaces, and endpoints for messages

---
+++ Tech-Trader-Server/Interfaces/IMessageService.cs
@@ -0,0 +1,13 @@
+﻿using TechTrader.Models;
+
+namespace TechTrader.Interfaces
+{
+    public interface IMessageService
+    {
+        Task<List<Message>> GetMessagesAsync();
+        Task<Message> GetMessageByIdAsync(int messageId);
+        Task<Message> CreateMessageAsync(Message Message);
+        Task<Message> UpdateMessageAsync(int messageId, Message Message);
+        Task<Message> DeleteMessageAsync(int messageId);
+    }
+}
---
+++ Tech-Trader-Server/Services/MessageService.cs
@@ -0,0 +1,40 @@
+﻿using TechTrader.Models;
+using TechTrader.Interfaces;
+
+namespace TechTrader.Services
+{
+    public class MessageService : IMessageService
+    {
+        private readonly IMessageRepository _messageRepository;
+
+        public MessageService(IMessageRepository messageRepository)
+        {
+            _messageRepository = messageRepository;
+        }
+
+        public async Task<List<Message>> GetMessagesAsync()
+        {
+            return await _messageRepository.GetMessagesAsync();
+        }
+
+        public async Task<Message> GetMessageByIdAsync(int messageId)
+        {
+            return await _messageRepository.GetMessageByIdAsync(messageId);
+        }
+
+        public async Task<Message> CreateMessageAsync(Message message)
+        {
+            return await _messageRepository.CreateMessageAsync(message);
+        }
+
+        public async Task<Message> UpdateMessageAsync(int messageId, Message message)
+        {
+            return await _messageRepository.UpdateMessageAsync(messageId, message);
+        }
+
+        public async Task<Message> DeleteMessageAsync(int messageId)
+        {
+            return await _messageRepository.DeleteMessageAsync(messageId);
+        }
+    }
+}
